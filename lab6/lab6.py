5832481f5ca0e789a83e40f04006123ee6c797e9
Chilaboc Ecaterina Mihaela <c_ecaterina_mihaela96@yahoo.com>
2018-02-08 16:29:26 +0200

    Next Commit

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,8 +1,57 @@
-    # Sa se scrie o functie care extrage cuvintele dintr-un text dat ca parametru. Un cuvant este definit ca o secventa de caractere alfa-numerice.
-    # Sa se scrie o functie care primeste ca parametru un sir de caractere regex, un sir de caractere text si un numar intreg x si returneaza acele substring-uri de lungime maxim x care fac match pe expresia regulata data.
-    # Sa se scrie o functie care primeste ca parametru un sir de caractere text si o lista de expresii regulate si returneaza o lista de siruri de caractere care fac match pe cel putin o expresie regulata data ca parametru.
-    # Sa se scrie o functie care primeste ca parametru path-ul catre un document xml si un dictionar attrs si returneaza acele elemente care au ca atribute toate cheile din dictionar si ca valoare valorile corespunzatoare. De exemplu, pentru dictionarul {"class": "url", "name": "url-form", "data-id": "item"} se vor selecta elementele care au atributele: class="url" si name="url-form" si data-id="item".
-    # Sa se scrie o alta varianta a functiei de la exercitiul anterior care returneaza acele elemente care au cel putin un atribut care corespunde cu o pereche cheie-valoare din dictionar.
-    # Sa se scrie o functie care pentru un text dat ca parametru, cenzureaza cuvintele care incep si se termina cu vocale. Prin cenzurare se intelege inlocuirea caracterelor de pe pozitii impare cu caracterul * .
-    # Sa se verifice, folosind o expresie regulata, daca un sir de caractere reprezinta un CNP valid.
-    # Sa se scrie o functie care parcurge recursiv un director si afiseaza acele fisiere a caror nume face match pe o expresie regulata data ca parametru sau contine un sir de caractere care face match pe aceeasi expresie. Fisierele care satisfac ambele conditii vor fi afisate prefixate cu ">>"
+# 1. Sa se scrie o functie care extrage cuvintele dintr-un text dat ca parametru. Un cuvant este definit ca o secventa de
+# caractere alfa-numerice.
+def exercitiul_1():
+    print("ok")
+print("Raspuns exerctiul 1:")
+exercitiul_1()
+
+# 2. Sa se scrie o functie care primeste ca parametru un sir de caractere regex, un sir de caractere text si un numar
+# intreg x si returneaza acele substring-uri de lungime maxim x care fac match pe expresia regulata data.
+def exercitiul_2():
+    print("ok")
+print("Raspuns exerctiul 2:")
+exercitiul_2()
+
+# 3. Sa se scrie o functie care primeste ca parametru un sir de caractere text si o lista de expresii regulate si
+# returneaza o lista de siruri de caractere care fac match pe cel putin o expresie regulata data ca parametru.
+def exercitiul_3():
+    print("ok")
+print("Raspuns exerctiul 3:")
+exercitiul_3()
+
+# 4. Sa se scrie o functie care primeste ca parametru path-ul catre un document xml si un dictionar attrs si returneaza
+# acele elemente care au ca atribute toate cheile din dictionar si ca valoare valorile corespunzatoare.
+# De exemplu, pentru dictionarul {"class": "url", "name": "url-form", "data-id": "item"} se vor selecta elementele care
+# au atributele: class="url" si name="url-form" si data-id="item".
+def exercitiul_4():
+    print("ok")
+print("Raspuns exerctiul 4:")
+exercitiul_4()
+
+# 5. Sa se scrie o alta varianta a functiei de la exercitiul anterior care returneaza acele elemente care au cel putin
+# un atribut care corespunde cu o pereche cheie-valoare din dictionar.
+def exercitiul_5():
+    print("ok")
+print("Raspuns exerctiul 5:")
+exercitiul_5()
+
+# 6. Sa se scrie o functie care pentru un text dat ca parametru, cenzureaza cuvintele care incep si se termina cu vocale.
+# Prin cenzurare se intelege inlocuirea caracterelor de pe pozitii impare cu caracterul * .
+def exercitiul_6():
+    print("ok")
+print("Raspuns exerctiul 6:")
+exercitiul_6()
+
+# 7. Sa se verifice, folosind o expresie regulata, daca un sir de caractere reprezinta un CNP valid.
+def exercitiul_7():
+    print("ok")
+print("Raspuns exerctiul 7:")
+exercitiul_7()
+
+# 8. Sa se scrie o functie care parcurge recursiv un director si afiseaza acele fisiere a caror nume face match pe o
+# expresie regulata data ca parametru sau contine un sir de caractere care face match pe aceeasi expresie. Fisierele care
+# satisfac ambele conditii vor fi afisate prefixate cu ">>"
+def exercitiul_8():
+    print("ok")
+print("Raspuns exerctiul 8:")
+exercitiul_8()
